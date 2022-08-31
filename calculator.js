e84b0ede66b68efbfb2b107a5bd6cd5b1b00c68d
Ghufran <ghufranshahid101@gmail.com>
2022-08-31 03:12:43 -0400

    Added eventlisteners for each number button. Added function to update display when buttons are clicked

---
+++ calculator.js
@@ -0,0 +1,18 @@
+
+
+let displayNum = "";
+const display = document.querySelector(".display-text");
+const numbers = document.querySelectorAll(".number-button");
+
+numbers.forEach(number => {
+    number.addEventListener('click', () => {updateDisplay(number.id)});
+});
+
+function updateDisplay(number){
+    
+    if (displayNum.length > 11){
+        displayNum += number;
+        display.textContent = displayNum;
+    }
+    
+}
