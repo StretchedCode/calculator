66e2d9d54e01b0d4e82f1fc29578efdc8e347216
Ghufran <ghufranshahid101@gmail.com>
2022-08-31 03:49:01 -0400

    Pass in values by setting the first number when clicking any of the operator buttons, and setting the second when the equal button is clicked

--- calculator.js
+++ calculator.js
@@ -1,8 +1,13 @@
 
 
 let displayNum = "";
+let a = 0;
+let b = 0;
+let operatorType;
 const display = document.querySelector(".display-text");
 const numbers = document.querySelectorAll(".number-button");
+const operators = document.querySelectorAll(".operator-button");
+const equal = document.querySelector("#equal");
 
 numbers.forEach(number => {
     number.addEventListener('click', () => {updateDisplay(number.id)});
@@ -16,3 +21,18 @@ function updateDisplay(number){
     }
     
 }
+
+operators.forEach(operator => {
+    operator.addEventListener('click', function setFirst(){
+        a = parseInt(displayNum);
+        operatorType = operator.id;
+        displayNum = "";
+        display.textContent = displayNum;
+    })
+});
+
+equal.addEventListener('click', () =>{
+
+    b = parseInt(displayNum);
+
+})
